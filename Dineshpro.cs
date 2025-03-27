6467d7db8593bcb81592bb695cdccbe1a5fa0d75
dineshkumar2407 <2423070@saec.ac.in>
2025-03-27 19:05:01 +0530

    Add files via upload

---
+++ Dineshpro.cs
@@ -0,0 +1,12 @@
+using System;
+class program
+{
+    static void main()
+    {
+        console.writeline("hello,welcome to visual studio!");
+        string name=console.readline();
+        console.writeline($"Nice to meet you,{name}!");
+        console.write("Press any key to exit...");
+        console.Readkey();
+    }
+}
